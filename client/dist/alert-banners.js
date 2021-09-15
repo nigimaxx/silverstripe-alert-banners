ecdb4a8c1043f033e2da67524874f1a012eb2a89
Shane Watters <shane.watters@dna.co.nz>
2021-09-16 11:43:49 +1200

    Rewriting JS to work with IE11.

--- client/dist/alert-banners.js
+++ client/dist/alert-banners.js
@@ -1,44 +1,43 @@
-class AlertBanners {
+var AlertBanners = {
     /*
      * Adds listeners to all alert banner close buttons on the current page.
      */
-    init() {
+    init: function() {
         const bannerButtons = document.querySelectorAll('.alertbanner__close');
 
         if (bannerButtons !== null) {
             var self = this
             for (let i = 0; i < bannerButtons.length; i++) {
                 const button = bannerButtons[i];
-                button.addEventListener('click', e => {
+                button.addEventListener('click', function(e) {
                         e.preventDefault();
                         const target = e.target,
                             bannerID = target.dataset['bannerId'],
                             cookieLength = target.dataset['cookieLength'],
                             banner = target.closest('.alertbanner');
                         self.setCookie(bannerID, cookieLength);
-                        console.log(banner);
                         self.hideBanner(banner);
                     }
                 );
             }
         }
-    }
+    },
 
     /*
      * Sets a cookie to hide the alert banner next time the user visits the page
      */
-    setCookie(bannerID, cookieLength){
-        const days = cookieLength ? cookieLength : 30,
+    setCookie: function(bannerID, cookieLength){
+        const days = parseInt(cookieLength) ? cookieLength : 30,
             millis = (days * 86400000), // number of days * milliseconds in a day
             date = new Date();
         date.setTime(date.getTime() + millis);
-        document.cookie = 'hidealertbanner-' + bannerID + '=true;expires=' + date.toUTCString() + ';path=' + window.location.origin + ';';
-    }
+        document.cookie = 'hidealertbanner-' + bannerID + '=true;expires=' + date.toUTCString() + ';path=/;';
+    },
 
     /*
      * Slides the up out of view when a dismissed by the user
      */
-    hideBanner(target) {
+    hideBanner: function(target) {
         const duration = 500;
         target.style.transitionProperty = 'height, margin, padding';
         target.style.transitionDuration = duration + 'ms';
@@ -51,7 +50,7 @@ class AlertBanners {
         target.style.paddingBottom = 0;
         target.style.marginTop = 0;
         target.style.marginBottom = 0;
-        window.setTimeout( () => {
+        window.setTimeout( function(){
             target.style.display = 'none';
             target.style.removeProperty('height');
             target.style.removeProperty('padding-top');
@@ -65,9 +64,8 @@ class AlertBanners {
     }
 }
 
-document.addEventListener('readystatechange', () => {
+document.addEventListener('readystatechange', function() {
     if (document.readyState === 'interactive') {
-        const alerts = new AlertBanners();
-        alerts.init();
+        AlertBanners.init();
     }
 });
