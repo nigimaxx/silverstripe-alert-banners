6cc5baefd80bac31e555935ed3ad19b073016976
Niklas Mack <nigimaxx@gmail.com>
2025-08-19 16:16:55 +1200

    Update module for resources in EditorConfig

--- src/Config/EditorConfig.php
+++ src/Config/EditorConfig.php
@@ -19,16 +19,15 @@ class EditorConfig
             'skin' => 'silverstripe'
         ]);
         // Enable insert-link to internal pages
-        $cmsModule = ModuleLoader::inst()->getManifest()->getModule('silverstripe/cms');
-        $adminModule = ModuleLoader::inst()->getManifest()->getModule('silverstripe/admin');
+        $tinyMceModule = ModuleLoader::inst()->getManifest()->getModule('silverstripe/htmleditor-tinymce');
         $editor->enablePlugins([
-            'sslinkinternal' => $cmsModule->getResource('client/dist/js/TinyMCE_sslink-internal.js'),
-            'sslinkanchor' => $cmsModule->getResource('client/dist/js/TinyMCE_sslink-anchor.js'),
+            'sslinkinternal' => $tinyMceModule->getResource('client/dist/js/TinyMCE_sslink-internal.js'),
+            'sslinkanchor' => $tinyMceModule->getResource('client/dist/js/TinyMCE_sslink-anchor.js'),
             'contextmenu' => null,
             'image' => null,
-            'sslink' => $adminModule->getResource('client/dist/js/TinyMCE_sslink.js'),
-            'sslinkexternal' => $adminModule->getResource('client/dist/js/TinyMCE_sslink-external.js'),
-            'sslinkemail' => $adminModule->getResource('client/dist/js/TinyMCE_sslink-email.js'),
+            'sslink' => $tinyMceModule->getResource('client/dist/js/TinyMCE_sslink.js'),
+            'sslinkexternal' => $tinyMceModule->getResource('client/dist/js/TinyMCE_sslink-external.js'),
+            'sslinkemail' => $tinyMceModule->getResource('client/dist/js/TinyMCE_sslink-email.js'),
         ])->setOption('contextmenu', 'sslink ssmedia ssembed inserttable | cell row column deletetable');
 
         $editor->removeButtons(
